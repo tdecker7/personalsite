9dcd09d3f8f243c9ed2a95f868e17d97ee6daa47
Trey Decker <treydecker@Treys-MacBook-Air.local>
2018-06-05 13:44:02 -0600

    smooth scroll, fonts, career page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,44 @@
 $(document).ready( function () {
-  $('#bike-pic').on('mouseover', function() {
-    console.log('This worked');
-    $('capt-slider').fadeIn();
+  var btn = $('.btn-group');
+  var origPosition = btn.position();
+  console.log(origPosition.top, origPosition.left);
+  var distance = btn.offset().top;
+  var   $window  = $(window);
+
+  var showFun = function () {
+    $('.spinner').css('display', 'block');
+    $('#fun').css('display', 'none');
+    console.log('Worked!');
+  }
+  var hideFun = function () {
+    $('.spinner').css('display', 'none');
+    $('#fun').css('display', 'inline-block');
+  }
+
+  $('#fun').on('click', showFun);
+  $('.spinner').on('click', hideFun);
+
+  var scroll = function() {
+    var elem = this;
+  }
+  $('#about-btn').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top
+    }, 1000);
+  });
+  /*
+  $window.scroll( function () {
+    if ($window.scrollTop() <= origPosition.top) {
+      btn.removeClass('sticky');
+    }
+    if ($window.scrollTop() >= distance) {
+      btn.animate({
+        top: 0,
+        right: ('-'+ origPosition.left)
+      }, 'slow');
+      btn.addClass('sticky');
+      console.log('at the top');
+    }
   })
+  */
 })
